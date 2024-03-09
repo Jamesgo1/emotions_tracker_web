8c20d4b31439afea67b70750749a3e447ceef576
Jamesgo1 <jimmygough3@gmail.com>
2024-03-09 16:21:26 +0000

    Add encryption for PII including passwords

--- controllers/emotionscontrollers.js
+++ controllers/emotionscontrollers.js
@@ -1,5 +1,7 @@
 const axios = require("axios");
 const bcrypt = require("bcrypt");
+const crypto = require("crypto");
+const {add} = require("nodemon/lib/rules");
 
 var cached_emotions_colors_arrays;
 
@@ -42,10 +44,73 @@ async function getDefaultHeaderData(req) {
     }
 }
 
+async function encryptPII(stringToEncrypt) {
+    const algorithm = 'aes-256-ctr';
+    const ENCRYPTION_KEY = process.env.ENCRYPT_KEY
+    resizedEncryptionKey = Buffer.concat([Buffer.from(ENCRYPTION_KEY), Buffer.alloc(32)], 32);
+    const IV_LENGTH = 16;
+
+    let iv = crypto.randomBytes(IV_LENGTH);
+    let cipher = crypto.createCipheriv(algorithm, resizedEncryptionKey, iv);
+    let encrypted = cipher.update(stringToEncrypt);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return iv.toString('hex') + ':' + encrypted.toString('hex');
+
+}
+
+async function decryptPII(objectToDecrypt) {
+    const algorithm = 'aes-256-ctr';
+    const ENCRYPTION_KEY = process.env.ENCRYPT_KEY
+    resizedEncryptionKey = Buffer.concat([Buffer.from(ENCRYPTION_KEY), Buffer.alloc(32)], 32);
+
+    let textParts = objectToDecrypt.split(':');
+    let iv = Buffer.from(textParts.shift(), 'hex');
+    let encryptedText = Buffer.from(textParts.join(':'), 'hex');
+    let decipher = crypto.createDecipheriv(algorithm, resizedEncryptionKey, iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+}
+
+async function addToLoginHist(successInd, userID) {
+
+    const loginHistEndpoint = 'http://localhost:3002/emotions/users/add-login-hist';
+
+    await axios
+        .post(loginHistEndpoint, {"success_ind": successInd, "user_id": userID})
+        .then((response) => {
+            console.log(response.data);
+        })
+        .catch((error) => {
+            console.log(`Error making API response: ${error}`);
+        });
+}
+
+async function getLoginAttempts(userID) {
+    console.log(`Input user ID: ${userID}`);
+
+    const loginAttemptsEndpoint = `http://localhost:3002/emotions/users/login-attempts/${userID}`
+    var outputResponse;
+    await axios
+        .get(loginAttemptsEndpoint, {
+            validateStatus: (status) => {
+                return status < 500
+            }
+        })
+        .then((response) => {
+            outputResponse = response;
+        })
+        .catch((error) => {
+            console.log(`Error making API response: ${error}`);
+        });
+    return outputResponse;
+}
+
 exports.getHome = async (req, res) => {
     headerParams = await getDefaultHeaderData(req)
     res.render("index", headerParams);
 };
+
 
 exports.getEmotionInfo = async (req, res) => {
     console.log(req.params);
@@ -104,43 +169,59 @@ exports.postLogin = async (req, res) => {
     var dataParams;
     const vals = {username, password} = req.body;
     console.log(vals);
-    const endpoint = `http://localhost:3002/emotions/users`;
+
+    // Return encrypted password if username exists:
+    const usernameEndpoint = 'http://localhost:3002/emotions/users/login-check';
+
     await axios
-        .post(endpoint, vals, {
+        .post(usernameEndpoint, {username}, {
             validateStatus: (status) => {
                 return status < 500
             }
         })
         .then(async (response) => {
-            const status = response.status;
-            // console.log(response);
-            console.log(status);
-            if (status === 200) {
-                const data = response.data;
-                console.log(data);
+            const usernameStatus = response.status;
+            console.log("Here's the username status:")
+            console.log(usernameStatus);
+            if (usernameStatus === 200) {
+                const returnedVals = response.data.data.output_data;
+                const dbPassword = returnedVals.password;
+                const dbuserid = returnedVals.user_id;
+                const decryptedPW = await decryptPII(dbPassword);
+                const totalFailedLogins = await getLoginAttempts(dbuserid);
+                if (totalFailedLogins.status !== 200) {
+                    await addToLoginHist(-1, dbuserid);
+                    dataParams = await getDefaultHeaderData(req);
+                    console.log("Too many failed logins status:")
+                    console.log(totalFailedLogins.status);
+                    dataParams["status_code"] = totalFailedLogins.status;
+                    res.render("login", dataParams);
+                } else {
+                    if (password === decryptedPW) {
+                        const session = req.session;
+                        session.isloggedin = true;
+                        session.userid = dbuserid;
+                        await addToLoginHist(1, dbuserid);
+                        res.redirect('/');
+                    } else {
+                        await addToLoginHist(0, dbuserid);
+                        dataParams = await getDefaultHeaderData(req);
+                        console.log("Incorrect password")
+                        console.log(usernameStatus);
+                        dataParams["status_code"] = 401;
+                        res.render("login", dataParams);
+                    }
 
-                const session = req.session;
-                session.isloggedin = true;
-                console.log(data);
-                session.userid = data.user_id;
-                console.log(session);
-                res.redirect('/');
-            } else if (status === 429) {
-                dataParams = await getDefaultHeaderData(req);
-                dataParams["status_code"] = status;
-                emotion_data = {emotion_name: ""};
-                res.render("login", dataParams);
-
+                }
             } else {
-                const data = response.data;
                 dataParams = await getDefaultHeaderData(req);
-                dataParams["status_code"] = status;
+                console.log("No username:")
+                console.log(usernameStatus);
+                dataParams["status_code"] = usernameStatus;
                 res.render("login", dataParams);
+
             }
         })
-        .catch((error) => {
-            console.log(`Error making API response: ${error}`);
-        });
 };
 
 exports.getEmotionsSubmitted = async (req, res) => {
@@ -210,42 +291,132 @@ async function checkRegCredentialsAvailable(usernameCheck, emailCheck) {
 exports.getRegister = async (req, res) => {
     var dataParams = await getDefaultHeaderData(req);
     dataParams["status_code"] = 0;
+    dataParams["validation_issues"] = [];
     res.render("register", dataParams);
 };
 
+async function permanentHashData(fieldToHash) {
+    const saltRounds = 8;
+    bcrypt
+        .hash(fieldToHash, saltRounds)
+        .catch((err) => {
+            console.log(`Hasing error: ${err}`);
+        })
+    return hash;
+}
+
+
+async function validatePassword(pw1, pw2) {
+    const passwordStatusArray = [];
+
+    if (pw1 !== pw2) {
+        passwordStatusArray.push("The two passwords do not match");
+    }
+    if (pw1.length < 7) {
+        passwordStatusArray.push("The password entered is less than seven characters");
+    }
+    if (pw1.indexOf(" ") >= 0) {
+        passwordStatusArray.push("The password contains spaces");
+    }
+    console.log(passwordStatusArray);
+    return passwordStatusArray;
+
+}
+
+async function validateUsername(inputField) {
+    const usernameStatusArray = [];
+    if (!inputField.match(/^[0-9a-zA-Z_-]+$/)) {
+        usernameStatusArray.push("Username contains characters that are not numbers, letters, dashes, or underscores");
+    }
+    return usernameStatusArray;
+
+}
+
+async function preprocessInput(jsObject) {
+    Object.keys(jsObject).forEach((key) => {
+        jsObject[key] = jsObject[key].trim();
+    })
+    return jsObject;
+}
+
+
 exports.postRegister = async (req, res) => {
-    const allRegisterVals = {username, password, password2, first_name, last_name, postcode, country, email} = req.body;
-    const valsToRegister = {username, password, first_name, last_name, postcode, country, email}
+    console.log("Encryption:")
+    var allRegisterVals = {username, password, password2, first_name, last_name, postcode, country, email} = req.body;
 
-    credAvailableResult = await checkRegCredentialsAvailable(username, email);
-    var dataParams;
+    const invalidStatusArray = [];
+    dataParams = await getDefaultHeaderData(req);
+    console.log("before:")
+    console.log(allRegisterVals)
+    allRegisterVals = await preprocessInput(allRegisterVals)
+    console.log(allRegisterVals)
 
-    if (credAvailableResult.status === 200) {
-        const endpoint = `http://localhost:3002/emotions/users/add-user-details`;
-        await axios
-            .post(endpoint, valsToRegister)
-            .then(async (response) => {
-                if (response.status === 200) {
+    const trimmedVals = {
+        username,
+        password,
+        password2,
+        first_name,
+        last_name,
+        postcode,
+        country,
+        email
+    } = await preprocessInput(allRegisterVals)
+    const invalidPasswordStatus = await validatePassword(password, password2);
+    console.log(invalidPasswordStatus)
+    invalidStatusArray.push(...invalidPasswordStatus);
+    const invalidUsernameStatus = await validateUsername(username);
+    invalidStatusArray.push(...invalidUsernameStatus);
 
-                    const session = req.session;
-                    session.isloggedin = true;
-                    session.userid = response.data.user_id;
-                    res.redirect('/');
-                } else {
-                    dataParams = await getDefaultHeaderData(req);
-                    dataParams["status_code"] = response.status;
-                    res.render("register", dataParams);
-                }
-
-            })
-            .catch((error) => {
-                console.log(`Error making API request: ${error}`)
-            });
-
-    } else {
-        dataParams = await getDefaultHeaderData(req);
-        dataParams["status_code"] = credAvailableResult.status;
+    if (invalidStatusArray.length > 0) {
+        console.log("Invalid statuses:")
+        console.log(invalidStatusArray);
+        dataParams["validation_issues"] = invalidStatusArray;
         res.render("register", dataParams);
+    } else {
+        const encryptedFirstName = await encryptPII(first_name);
+        const encryptedLastName = await encryptPII(last_name);
+        const encryptedPassword = await encryptPII(password);
+        const encryptedEmail = await encryptPII(email);
+        const valsToRegister = {
+            username,
+            encryptedPassword,
+            encryptedFirstName,
+            encryptedLastName,
+            postcode,
+            country,
+            encryptedEmail
+        }
+
+        dataParams = await getDefaultHeaderData(req);
+        credAvailableResult = await checkRegCredentialsAvailable(username, email);
+
+        if (credAvailableResult.status === 200) {
+            const endpoint = `http://localhost:3002/emotions/users/add-user-details`;
+            await axios
+                .post(endpoint, valsToRegister)
+                .then(async (response) => {
+                    if (response.status === 200) {
+
+                        const session = req.session;
+                        session.isloggedin = true;
+                        session.userid = response.data.user_id;
+                        res.redirect('/');
+                    } else {
+
+                        dataParams["validation_issues"] = ["Unknown issue with input provided"];
+                        res.render("register", dataParams);
+                    }
+
+                })
+                .catch((error) => {
+                    console.log(`Error making API request: ${error}`)
+                });
+
+        } else {
+            dataParams["validation_issues"] = ["Username and/or email already in use. Please use different" +
+            " credentials."];
+            res.render("register", dataParams);
+        }
     }
 };
 
